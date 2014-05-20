5b4636b0b767e94386685e047824c01be7e35d3b
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-05-20 02:07:14 +0200

    * pathfix * additional experiments

--- identificationTraining/expv3.m
+++ identificationTraining/expv3.m
@@ -2,14 +2,13 @@ addpath( genpath( getMFilePath() ) );
 addpath( genpath( [getMFilePath() '../'] ) );
 addpath( genpath( [getMFilePath() '../../tools/'] ) );
 
-
 ns = makeTrainingState();
 
 ns.angles = [0];
 
-ns.featureFunction = @msFeatures;
+ns.featureFunction = @polyFeatures;
 ns.featureFunctionParam.derivations = 1;
 
-ns.name = 'v1';
+ns.name = 'v3';
 
 produceModel( '../../../testSoundsIEEE_AASP', 'knock', ns );
