3f349b8dca89db66a17cb44ea679f6e042623470
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-06-06 16:03:57 +0200

    * identificationTraining: ** scale before each training / testing separately (more realistic than on whole training/testing data set) ** set training weight for positive examples before each training according to the actual proportion ** evaluate performance on training data too

--- identificationTraining/libsvmPredictExt.m
+++ identificationTraining/libsvmPredictExt.m
@@ -1,6 +1,9 @@
-function [pred ret dec] = libsvmPredictExt(y, x, model)
-[pred acc dec] = libsvmpredict(y, x, model);
+function [pred, val, dec] = libsvmPredictExt( y, x, model, translators, factors )
+
+x = scaleData( x, translators, factors );
+
+[pred, ~, dec] = libsvmpredict(y, x, model);
 if model.Label(1) < 0;
   dec = dec * -1;
 end
-ret = validation_function(dec, y);
+val = validation_function(dec, y);
