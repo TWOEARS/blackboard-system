c071d4ae45d354bbdb49e37af23de0dc77f4c746
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-06-06 16:03:57 +0200

    * identificationTraining: ** scale before each training / testing separately (more realistic than on whole training/testing data set) ** set training weight for positive examples before each training according to the actual proportion ** evaluate performance on training data too

--- identificationTraining/trainSvm.m
+++ identificationTraining/trainSvm.m
@@ -1,17 +1,15 @@
-function model = trainSvm( tridx, lfolds, dfolds, cvFolds, niState )
+function [model, translators, factors, predGenVal, hps, trVal] = trainSvm( foldsIdx, lfolds, dfolds, idfolds, niState )
 
+%% search for the best hyperparameters, using the folds indexed by foldsIdx
 
+hps = gridSvmTrain( foldsIdx, lfolds, dfolds, idfolds, niState );
+predGenVal = hps(end,5);
+
+%% train with the best hyperparameters, using all folds
+
+svmParamString = sprintf( '-t %d -g %e -c %e -w-1 1 -w1 %e -q -e %e', hps(end,1), hps(end,4), hps(end,3), 1, hps(end,2) );
 disp( '' );
-disp( 'grid search for best hyperparameters' );
-disp( '' );
-if ~isempty(strfind( lower(niState.hyperParamSearch), 'fast' ))
-    ridx = tridx(randi( length(tridx) ));
-    [~, bk, bg, bc, bcp, vals] = gridSvmTrain( dfolds{ridx}, lfolds{ridx}, cvFolds, niState );
-else
-    [~, bk, bg, bc, bcp, vals] = gridSvmTrain( vertcat( dfolds{tridx} ), vertcat( lfolds{tridx} ), cvFolds, niState );
-end
-svmParamString = sprintf( '-t %d -g %e -c %e -w-1 1 -w1 %e -q -e %e', bk, bg, bc, bcp, niState.terminationTolerance );
-disp( '' );
-disp( 'training with best hyperparameters' );
-model = libsvmtrain( vertcat( lfolds{tridx} ), vertcat( dfolds{tridx} ), svmParamString );
+fprintf( '\ntraining with best hyperparameters (CV gen prediction: %g)\n', predGenVal );
+
+[model, translators, factors, trVal] = libsvmtrainExt( vertcat( lfolds{foldsIdx} ), vertcat( dfolds{foldsIdx} ), svmParamString );
 
