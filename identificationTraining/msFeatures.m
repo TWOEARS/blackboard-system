02fb9e9e1e509b97f5592d6b673dc309118fc5e3
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-06-06 15:53:17 +0200

    * identificationTraining: use mean of L and R channel for msFeatures

--- identificationTraining/msFeatures.m
+++ identificationTraining/msFeatures.m
@@ -1,6 +1,7 @@
 function features = msFeatures( param, rmBlock )
 
-s = rmBlock(:,:,2);
+% TODO: determine which channel to use!
+s = .5 * rmBlock(:,:,1) + .5 * rmBlock(:,:,2);
 features = [mean( s, 2 ); std( s, 0, 2 )];
 for i = 1:param.derivations
     s = s(:,2:end) - s(:,1:end-1);
