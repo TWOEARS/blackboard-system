1cd0613c19ccb640b7f6fcf84ef415ee9b003e8c
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-06-06 16:07:09 +0200

    * identificationTraining: modified inner CV to be stratified too

--- identificationTraining/libsvmCVext.m
+++ identificationTraining/libsvmCVext.m
@@ -1,23 +1,24 @@
-function val = libsvmCVext(y, x, param, nr_fold, bestVal)
-len = length(y);
-rand_ind = randperm(len);
-dec_values = [];
-labels = [];
+function meanval = libsvmCVext( y, x, ids, param, nr_fold, bestVal )
+
+[lfolds, dfolds, ~] = splitDataPermutation( y, x, ids, nr_fold );
+
+vals = ones( nr_fold, 1 );
+trVals = ones( nr_fold, 1 );
 for i = 1:nr_fold % Cross training : folding
-  test_ind = rand_ind([floor((i-1)*len/nr_fold)+1:floor(i*len/nr_fold)]');
-  train_ind = [1:len]';
-  train_ind(test_ind) = [];
-  model = libsvmtrain(y(train_ind),x(train_ind,:),param);
-  [pred, acc, dec] = libsvmpredict(y(test_ind),x(test_ind,:),model);
-  if model.Label(1) < 0;
-    dec = dec * -1;
-  end
-  dec_values = vertcat(dec_values, dec);
-  labels = vertcat(labels, y(test_ind));
-  disp( sprintf( 'Validation value after run %d:', i ) );
-  val = validation_function(dec_values, labels);
-  if (i < nr_fold)  &&  ((val*i + 1*(nr_fold-i))/nr_fold <= bestVal)
-      disp( 'CV run cannot reach best value any more, aborting' );
-      break;
-  end
+    tridx = 1:nr_fold;
+    tridx(i) = [];
+
+    [model, translators, factors, trVals(i)] = libsvmtrainExt( vertcat( lfolds{tridx} ), vertcat( dfolds{tridx} ), param );
+    disp( 'testing performance:' );
+    [~, vals(i), ~] = libsvmPredictExt( lfolds{i}, dfolds{i}, model, translators, factors );
+  
+    meanval = mean(vals);
+    meantrval = mean(trVals);
+    if (i < nr_fold) && (meanval <= bestVal)
+        disp( 'CV run cannot reach best value any more, aborting' );
+        break;
+    end
 end
+
+fprintf( 'Average training performance after CV: %g\n', meantrval );
+fprintf( 'Average generalization performance after CV: %g\n', meanval );
