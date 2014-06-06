6144889c6765393a95d43ecaeaa8d3c8ae7d3b73
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-06-06 15:49:20 +0200

    * identificationTraining: makeTrainingState -> setupExperiment. Changed structure to be more modular. Modified according hash-creating functions.

--- identificationTraining/createNiStateModelId.m
+++
@@ -1,3 +0,0 @@
-function id = createNiStateModelId( niState )
-
-id = DataHash( {createNiStateSplitDataId( niState ) niState.hyperParamSearch niState.terminationTolerance niState.searchBudget} );
--- identificationTraining/createNiStateWP2id.m
+++
@@ -1,3 +0,0 @@
-function id = createNiStateWP2id( niState )
-
-id = DataHash( {niState.simParams.fsHz niState.simParams.nErbs niState.simParams.nChannels niState.simParams.mEarF niState.simParams.fLowHz niState.simParams.fHighHz niState.simParams.ihcMethod niState.simParams.winSizeSec niState.simParams.hopSizeSec niState.simParams.winType niState.simParams.blockSize niState.hopsPerShift niState.strCues niState.strFeatures niState.angles} );
