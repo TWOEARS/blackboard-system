aebfe72e907b395cc14516517bb2cd70e44ef194
Christopher Schymura <christopher.schymura@rub.de>
2014-07-04 10:39:30 +0200

    Further cleanup within stage1integration branch.

---
+++ src/gm_training/initTrainingScene.m
@@ -0,0 +1,30 @@
+function [scene, sourcePos, out] = initTrainingScene(sceneDuration, azimuth, sp)
+
+%% Error handling
+
+
+if ~isa(sp, 'struct')
+    error('Invalid simulation parameters.')
+end
+
+%% Define scene
+
+% Source signals and positions
+s1Pos = azimuth;
+s1 = SoundSource('TrainingData', 'training.wav', 'Polar', [1, s1Pos]);
+
+% Define dummy head
+dummyHead = Head('QU_KEMAR_anechoic_3m.mat', sp.fsHz);
+
+% Create scene
+scene = Scene(sceneDuration, sp.fsHz, sp.blockSize * sp.fsHz, ...
+    sp.blockSize * sp.fsHz, dummyHead, s1);
+
+% Return source position
+sourcePos = s1Pos;
+
+% Allocate output signal
+out = zeros(scene.numSamples + sp.blockSize * sp.fsHz + ...
+    dummyHead.numSamples - 1, 2);
+
+disp(['Source position: ', num2str(s1Pos)]);
