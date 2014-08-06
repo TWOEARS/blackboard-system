e496b55b62bdd1059c2654d3592135701e1677e2
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-08-06 11:08:10 +0200

    Suppressing output of jtcommand with >/dev/null

--- src/gmtk_matlab_interface/gmtkEngine.m
+++ src/gmtk_matlab_interface/gmtkEngine.m
@@ -258,7 +258,7 @@ classdef gmtkEngine < handle
                     fprintf(fid, '        -cliqueOutputFileName %s \\\n', obj.outputCliqueFile);
                     fprintf(fid, '        -cliqueListFileName %s \\\n', obj.outputCliqueFile);
                     fprintf(fid, '        -verbosity 0 \\\n');
-                    fprintf(fid, '        -cliquePrintFormat ascii\n');
+                    fprintf(fid, '        -cliquePrintFormat ascii > /dev/null\n');
                     fprintf(fid, '\n');
                     fclose(fid);
                     unix(['chmod a+x ' cmdfn]);
