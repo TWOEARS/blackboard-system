c70d303b08cd23d836d9894074aaddbc775f1a10
Christopher Schymura <christopher.schymura@rub.de>
2014-08-13 10:32:24 +0200

    Added first version of sharpening the ears test script.

--- test/add_WP_paths.m
+++ test/add_WP_paths.m
@@ -1,4 +1,4 @@
-function add_WP_paths
+function reporoot = add_WP_paths
 %
 % This script initialises the path variables that are needed for running
 % the WP3 testing code.
@@ -9,6 +9,10 @@ reporoot = fileparts(fileparts(fileparts(mfilename('fullpath'))));
 addpath(fullfile(reporoot, 'twoears-wp1/src'));
 startWP1;
 
+addpath(genpath(fullfile(reporoot, ...
+    'twoears-data/sound_databases/grid_subset/wav')));
 addpath(genpath(fullfile(reporoot, 'twoears-wp2/src')));
-
 addpath(genpath(fullfile(reporoot, 'twoears-wp3/src')));
+addpath(genpath(fullfile(reporoot, 'twoears-wp4/src')));
+addpath(genpath(fullfile(reporoot, 'twoears-wp5/src')));
+addpath(genpath(fullfile(reporoot, 'twoears-ssr/mex')));
