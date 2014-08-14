21c8c6abd392507c6723e1d4c1b22706772f96cc
Christopher Schymura <christopher.schymura@rub.de>
2014-08-14 16:48:22 +0200

    Added demo script for STE simulation.

--- test/add_WP_paths.m
+++ test/add_WP_paths.m
@@ -9,12 +9,13 @@ reporoot = fileparts(fileparts(fileparts(mfilename('fullpath'))));
 addpath(fullfile(reporoot, 'twoears-wp1/src'));
 startWP1;
 
-addpath(genpath(fullfile(reporoot, ...
-    'twoears-data/sound_databases/grid_subset/wav')));
-addpath(genpath(fullfile(reporoot, ...
-    'twoears-data/sound_databases/stimuli/binaural')));
+% addpath(genpath(fullfile(reporoot, ...
+%     'twoears-data/sound_databases/grid_subset/wav')));
+% addpath(genpath(fullfile(reporoot, ...
+%     'twoears-data/sound_databases/stimuli/binaural')));
+addpath(genpath(fullfile(reporoot, 'twoears-data')));
 addpath(genpath(fullfile(reporoot, 'twoears-wp2/src')));
-%addpath(genpath(fullfile(reporoot, 'twoears-wp3/src')));
+addpath(genpath(fullfile(reporoot, 'twoears-wp3/src')));
 addpath(genpath(fullfile(reporoot, 'twoears-wp4/src')));
 addpath(genpath(fullfile(reporoot, 'twoears-wp5/src')));
 addpath(genpath(fullfile(reporoot, 'twoears-ssr/mex')));
---
+++ test/test_ste/test_ste_demo.m
@@ -0,0 +1,28 @@
+clear all;
+close all;
+clc;
+
+% Add necessary paths
+addpath('..');
+repoRoot = add_WP_paths;
+
+% Create an instance of the simulation wrapper class
+testWrapper = SimulationWrapper('test_ste.xml');
+
+% Specify audio file paths
+file1 = fullfile('/sound_databases/grid_subset/wav/s1/bbas2p.wav');
+file2 = fullfile('/sound_databases/grid_subset/wav/s2/bbie8s.wav');
+file3 = fullfile('/stimuli/binaural/binaural_forest.wav');
+
+% Add files to the class instance
+testWrapper.addTargetSignal(file1, 90);
+testWrapper.addMaskerSignal(file2, 270);
+testWrapper.addNoiseSignal(noise);
+
+% Generate output signal
+targetToMaskerSNR = -15; % Target-to-Masker SNR in dB
+targetToNoiseSNR = -15; % Target-to-Noise SNRin dB
+out = testWrapper.renderSignals(targetToMaskerSNR, targetToNoiseSNR);
+
+% Play output signal
+soundsc(out, 44100)
