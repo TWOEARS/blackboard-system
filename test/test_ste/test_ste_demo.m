af7f7c6795b89e388c4124f6eb7f63bda3677bf7
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-08-28 16:22:57 +0200

    * adapted test_ste_demo.m to use database now located in data git. * changed basic_timestep to have shorter processing time... * changed test scene: now 50 seconds long, fire in bg, baby starts after 5 seconds, after 30 seconds overlap of baby and female, after 35 seconds only female. This scene is already precompiled in the data git!

--- test/test_ste/test_ste_demo.m
+++ test/test_ste/test_ste_demo.m
@@ -1,7 +1,7 @@
 % Add necessary paths
 addpath('..');
 repoRoot = add_WP_paths;
-addpath(genpath(fullfile(repoRoot, 'TwoEarsRUB')));
+addpath(genpath(fullfile(repoRoot, 'dataGit/sound_databases')));
 
 %clearAllButBreakpoints;
 close all;
@@ -11,7 +11,7 @@ clc;
 %% load ste scene
 
 fs = 44100;
-basicTimeStep = 0.02; % for scene processing
+basicTimeStep = 0.1; % for scene processing
 
 if ~exist( 'ste.mat', 'file' )
     
@@ -20,26 +20,26 @@ if ~exist( 'ste.mat', 'file' )
     testWrapper = SimulationWrapper('test_ste.xml');
     
     % Specify audio file paths
-    file1 = fullfile('/TwoEarsRUB/baby.wav');
-    file2 = fullfile('/TwoEarsRUB/test/femaleSpeech/bbad2n.wav');
-    file3 = fullfile('/TwoEarsRUB/test/fire/FireHouse+CRB02_24.wav');
+    file1 = fullfile('HumanBaby+6105_97_1.wav');
+    file2 = fullfile('femaleTest.wav');
+    file3 = fullfile('fireTest.wav');
     
     % Add files to the class instance
-    testWrapper.addTargetSignal(file1, 0);
-    testWrapper.addMaskerSignal(file2, 270);
+    testWrapper.addTargetSignal(file1, 0, 5 );
+    testWrapper.addMaskerSignal(file2, 270, 30 );
     testWrapper.addNoiseSignal(file3);
     
     [lt, lm, ln] = testWrapper.renderLabels( 1 / basicTimeStep );
     % Generate output signal
-    targetToMaskerSNR = 20; % Target-to-Masker SNR in dB
-    targetToNoiseSNR = 300; % Target-to-Noise SNR in dB
+    targetToMaskerSNR = 0; % Target-to-Masker SNR in dB
+    targetToNoiseSNR = 10; % Target-to-Noise SNR in dB
     steSound = testWrapper.renderSignals(targetToMaskerSNR, targetToNoiseSNR);
     
     % save for later use
-    save( [repoRoot filesep 'TwoEarsRUB' filesep 'ste.mat'], 'steSound', 'fs', 'lt', 'ln', 'lm', 'targetToMaskerSNR', 'targetToNoiseSNR', 'file1', 'file2', 'file3' );
+    save( [repoRoot filesep 'dataGit/sound_databases/renderedScenes' filesep 'ste.mat'], 'steSound', 'fs', 'lt', 'ln', 'lm', 'targetToMaskerSNR', 'targetToNoiseSNR', 'file1', 'file2', 'file3' );
     
 else
-    load( [repoRoot filesep 'TwoEarsRUB' filesep 'ste.mat'], 'steSound', 'fs', 'lt', 'ln', 'lm', 'targetToMaskerSNR', 'targetToNoiseSNR', 'file1', 'file2', 'file3' );    
+   load( [repoRoot filesep 'dataGit/sound_databases/renderedScenes' filesep 'ste.mat'], 'steSound', 'fs', 'lt', 'ln', 'lm', 'targetToMaskerSNR', 'targetToNoiseSNR', 'file1', 'file2', 'file3' );    
 end
 
 %% Play output signal
@@ -109,7 +109,6 @@ if length(femaleProbs) < sceneLen_steps
     femaleProbs = [femaleProbs zeros(1,sceneLen_steps-length(femaleProbs))];
 end
 t = (1:sceneLen_steps) * basicTimeStep;
-%plot( t, babyProbs, t, femaleProbs, t, lt, t, lm );
 
 figure1 = figure( 'Name','label probabilities' );
 axes1 = axes( 'Parent',figure1,'YTickLabel',{'0','0,33','0,5','0,66','1'},...
@@ -121,15 +120,15 @@ box( axes1,'on' );
 hold( axes1,'all' );
 plot1 = plot( t, babyProbs, t, femaleProbs, t, lt, t, lm ,'Parent',axes1,'LineWidth',2);
 set( plot1(1),...
-    'Color',[0.0431372560560703 0.517647087574005 0.780392169952393],...
+    'Color',[0.05 0.5 0.8],...
     'DisplayName','baby model' );
-set( plot1(2),'Color',[0.847058832645416 0.160784319043159 0],...
+set( plot1(2),'Color',[0.05 0.8 0.5],...
     'DisplayName','female model' );
 set( plot1(3),'LineStyle',':',...
-    'Color',[0.0431372560560703 0.517647087574005 0.780392169952393],...
+    'Color',[0.05 0.5 0.8],...
     'DisplayName','baby true' );
 set( plot1(4),'LineStyle',':',...
-    'Color',[0.847058832645416 0.160784319043159 0],...
+    'Color',[0.05 0.8 0.5],...
     'DisplayName','female true' );
 title( 'Label Probabilities' );
 xlabel( 't (s)' );
