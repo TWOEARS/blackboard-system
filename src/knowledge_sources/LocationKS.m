b8b63c575fd9c44d596c50a956e7c7608be213e9
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-08-31 16:50:26 +0200

    * changed blockNo of LocationHypothesis to be index of last chunk of processed data * headrotation is now only saved in the blackboard. <= at the moment, "overlapping" information cannot occur, all action is performed in each cycle of the scheduler. Once this changes, a history can be introduced.

--- src/knowledge_sources/LocationKS.m
+++ src/knowledge_sources/LocationKS.m
@@ -74,7 +74,7 @@ classdef LocationKS < Wp2DepKS
             
             % We simply take the average of posteriors across all the
             % samples for this block
-            locHyp = LocationHypothesis(acousticCues.blockNo, acousticCues.headOrientation, obj.angles, mean(post,1));
+            locHyp = LocationHypothesis(ildsSObj.LastChunk(2), obj.blackboard.headOrientation, obj.angles, mean(post,1));
             idx = obj.blackboard.addLocationHypothesis(locHyp);
             notify(obj.blackboard, 'NewLocationHypothesis', BlackboardEventData(idx));
         end
