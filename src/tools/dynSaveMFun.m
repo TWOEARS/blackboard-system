1c3c9d357217f6e2c3a5eab1714eed941feb761f
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-09-30 17:00:03 +0200

    tools should not be there any more, we have a tools repo

--- src/tools/dynSaveMFun.m
+++
@@ -1,5 +0,0 @@
-function dynSaveMFun( functionHandle, param, matFile )
-f = functions( functionHandle );
-urlname = ['file:///' f.file];
-fstr = urlread( urlname );
-save( matFile, 'fstr', 'f', 'param' );
