53ab85610764596d820e0aa9cacd7753224e24b9
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-09-30 19:48:31 +0200

    calculating also block-based id error rate

--- src/evaluation/IdEvalFrame.m
+++ src/evaluation/IdEvalFrame.m
@@ -20,9 +20,9 @@ classdef IdEvalFrame < handle
             obj.idTruth.onsetsOffsets(obj.idTruth.onsetsOffsets(:,2) == inf,:) = [];
         end
         
-        function ide = calcIdError( obj, idClass )
+        function ide = calcIdError( obj )
             idDecs = obj.bb.getData( 'identityDecision' );
-            idDecs = idDecs(arrayfun(@(x)(strcmp(x.data.label,idClass)),idDecs));
+            idDecs = idDecs(arrayfun(@(x)(strcmp(x.data.label,obj.idTruth.class)),idDecs));
             if ~isempty( idDecs )
                 iddTm = [idDecs.sndTmIdx];
                 iddDat = [idDecs.data];
@@ -40,24 +40,56 @@ classdef IdEvalFrame < handle
                 end
             end
             
-            ide.testposTime = sum( iddOnOffs(:,2) - iddOnOffs(:,1) );
-            ide.testnegTime = obj.bb.currentSoundTimeIdx - ide.testposTime;
-            ide.condposTime = sum( obj.idTruth.onsetsOffsets(:,2) - obj.idTruth.onsetsOffsets(:,1) );
-            ide.condnegTime = obj.bb.currentSoundTimeIdx - ide.condposTime;
-            ide.trueposTime = 0;
+            ide.time.testpos = sum( iddOnOffs(:,2) - iddOnOffs(:,1) );
+            ide.time.testneg = obj.bb.currentSoundTimeIdx - ide.time.testpos;
+            ide.time.condpos = sum( obj.idTruth.onsetsOffsets(:,2) - obj.idTruth.onsetsOffsets(:,1) );
+            ide.time.condneg = obj.bb.currentSoundTimeIdx - ide.time.condpos;
+            ide.time.truepos = 0;
             for k = 1:size(iddOnOffs,1)
                 intersectOffs = min( iddOnOffs(k,2), obj.idTruth.onsetsOffsets(:,2) );
                 intersectOns = max( iddOnOffs(k,1), obj.idTruth.onsetsOffsets(:,1) );
                 overlaps = max( 0, intersectOffs - intersectOns );
-                ide.trueposTime = ide.trueposTime + sum( overlaps );
+                ide.time.truepos = ide.time.truepos + sum( overlaps );
             end
-            ide.falseposTime = ide.testposTime - ide.trueposTime;
-            ide.truenegTime = ide.condnegTime - ide.falseposTime;
-            ide = IdEvalFrame.meanErrors( ide );
+            ide.time.trueneg = ide.time.condneg - ide.time.testpos + ide.time.truepos;
+            ide.time = IdEvalFrame.meanErrors( ide.time );
+            
+            ide.blocks.condpos = 0;
+            ide.blocks.condneg = 0;
+            ide.blocks.testpos = 0;
+            ide.blocks.truepos = 0;
+            ide.blocks.testneg = 0;
+            ide.blocks.trueneg = 0;
+            idHyps = obj.bb.getData( 'identityHypotheses' );
+            for idHyp = idHyps
+                endBlockTime = idHyp.sndTmIdx;
+                startBlockTime = endBlockTime - idHyp.data(1).concernsBlocksize_s;
+                blockInclEvent = ...
+                    ( sum( (obj.idTruth.onsetsOffsets(:,1) <= endBlockTime) ...
+                            == (obj.idTruth.onsetsOffsets(:,2) >= endBlockTime) ) ...
+                    + sum( (obj.idTruth.onsetsOffsets(:,1) <= startBlockTime) ...
+                            == (obj.idTruth.onsetsOffsets(:,2) >= startBlockTime) ) ...
+                    + sum( (obj.idTruth.onsetsOffsets(:,1) >= startBlockTime) ...
+                            == (obj.idTruth.onsetsOffsets(:,2) <= endBlockTime) ) )...
+                    >= 1;
+                ide.blocks.condpos = ide.blocks.condpos + blockInclEvent;
+                ide.blocks.condneg = ide.blocks.condneg + ~blockInclEvent;
+
+                idDec = obj.bb.getData( 'identityDecision', idHyp.sndTmIdx );
+                if ~isempty( idDec ) && strcmpi( obj.idTruth.class, idDec.data.label )
+                    ide.blocks.testpos = ide.blocks.testpos + 1;
+                    ide.blocks.truepos = ide.blocks.truepos + blockInclEvent;
+                else
+                    ide.blocks.testneg = ide.blocks.testneg + 1;
+                    ide.blocks.trueneg = ide.blocks.trueneg + ~blockInclEvent;
+                end
+            end
+            ide.blocks = IdEvalFrame.meanErrors( ide.blocks );
+            
             obj.idError = ide;
         end
     end
-   
+    
     %% Static utils
     methods (Static)
         
@@ -87,19 +119,19 @@ classdef IdEvalFrame < handle
         end
         
         function errors = meanErrors( errors )
-            errors(1).trueposTime = sum([errors.trueposTime]);
-            errors(1).condposTime = sum([errors.condposTime]);
-            errors(1).condnegTime = sum([errors.condnegTime]);
-            errors(1).testposTime = sum([errors.testposTime]);
-            errors(1).truenegTime = sum([errors.truenegTime]);
-            errors(1).testnegTime = sum([errors.testnegTime]);
+            errors(1).truepos = sum([errors.truepos]);
+            errors(1).condpos = sum([errors.condpos]);
+            errors(1).condneg = sum([errors.condneg]);
+            errors(1).testpos = sum([errors.testpos]);
+            errors(1).trueneg = sum([errors.trueneg]);
+            errors(1).testneg = sum([errors.testneg]);
             errors(2:end) = [];
-            errors.sensitivity = errors.trueposTime / errors.condposTime;
-            errors.pospredval = errors.trueposTime / errors.testposTime;
-            errors.specificity = errors.truenegTime / errors.condnegTime;
-            errors.negpredval = errors.truenegTime / errors.testnegTime;
-            errors.acc = (errors.trueposTime + errors.truenegTime) / ...
-                (errors.condposTime + errors.condnegTime);
+            errors.sensitivity = errors.truepos / errors.condpos;
+            errors.pospredval = errors.truepos / errors.testpos;
+            errors.specificity = errors.trueneg / errors.condneg;
+            errors.negpredval = errors.trueneg / errors.testneg;
+            errors.acc = (errors.truepos + errors.trueneg) / ...
+                (errors.condpos + errors.condneg);
         end
         
     end
