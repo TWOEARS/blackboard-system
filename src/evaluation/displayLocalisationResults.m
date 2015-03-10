fc51c1e5fd041f333ee09bb74f97e40ae396d5b5
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-03-10 13:07:10 +0100

    Additional evaluation function for localisation results

--- src/evaluation/displayLocalisationResults.m
+++ src/evaluation/displayLocalisationResults.m
@@ -13,7 +13,7 @@ if nargin<2
 end
 
 fprintf('\n------------------------------------------------------------------------------------\n');
-fprintf('Reference target angle: %d degrees\n', sourceAzimuth);
+fprintf('Reference target angle: %3.0f degrees\n', sourceAzimuth);
 fprintf('------------------------------------------------------------------------------------\n');
 fprintf('Localised source angle:\n');
 %fprintf('------------------------------------------------------------------------------------\n');
@@ -23,21 +23,15 @@ fprintf('-----------------------------------------------------------------------
 estAngles = zeros(length(perceivedLocations),1);
 
 for m=1:length(perceivedLocations)
-
-    relativeLocation = mod( perceivedLocations(m).data.location + ...
-        perceivedLocations(m).data.headOrientation, 360 );
-
     fprintf('% 6.2f\t\t%3.0f degrees\t\t(%3.0f degrees,    %3.0f degrees)\t%.2f\n', ...
         perceivedLocations(m).sndTmIdx, ...
-        relativeLocation, ...
+        perceivedLocations(m).data.relativeLocation, ...
         perceivedLocations(m).data.headOrientation, ...
         perceivedLocations(m).data.location, ...
         perceivedLocations(m).data.score);
-
-    estAngles(m) = relativeLocation;
 end
 fprintf('------------------------------------------------------------------------------------\n');
-locError = mean(localisationErrors(sourceAzimuth, estAngles));
+[~, locError] = evaluateLocalisationResults(perceivedLocations, sourceAzimuth);
 fprintf('Mean localisation error: %g\n', locError );
 fprintf('------------------------------------------------------------------------------------\n\n');
 
