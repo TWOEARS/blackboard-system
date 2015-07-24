64c74145d47ef4c325e924665acfd4b28502f6dc
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2015-07-24 03:34:03 +0200

    identification evaluation tools

--- src/evaluation/plotIdentificationScene.m
+++ src/evaluation/plotIdentificationScene.m
@@ -19,22 +19,7 @@ for ii = 1 : length(labelsTrunc)
     text( onOffsetsTrunc(ii,1), 1.03, labelsTrunc{ii}, 'Parent', axh );
 end
 
-idScores = struct();
-for ii = numel( identityHypotheses ) : -1 : 1
-    off = identityHypotheses(ii).sndTmIdx;
-    for jj = 1 : numel( identityHypotheses(ii).data )
-        on = max( 0, off - identityHypotheses(ii).data(jj).concernsBlocksize_s );
-        score = identityHypotheses(ii).data(jj).p;
-        label = identityHypotheses(ii).data(jj).label;
-        if isfield( idScores, label )
-            idScores.(label).x = [on idScores.(label).x];
-            idScores.(label).y = [score idScores.(label).y];
-        else
-            idScores.(label).x = [on off];
-            idScores.(label).y = [score score];
-        end
-    end
-end
+idScores = getIdScores( identityHypotheses );
 
 idLabels = sort( fieldnames( idScores ) );
 for ii = 1 : numel( idLabels )
