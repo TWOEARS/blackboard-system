9c38bd8f7d399a6a3493de5b079db4574823d4a5
Youssef Kashef <youssef.kashef@gmail.com>
2016-09-13 17:00:52 +0200

    add decision property to identity hypothesis and populate it with model decision

--- src/blackboard_data/IdentityHypothesis.m
+++ src/blackboard_data/IdentityHypothesis.m
@@ -2,15 +2,17 @@ classdef IdentityHypothesis < Hypothesis
     
     properties (SetAccess = private)
         label;
-        p;
+        p;                      % probability
+        d;                      % decision
         concernsBlocksize_s;
     end
     
     methods
-        function obj = IdentityHypothesis( label, p, blocksize_s )
+        function obj = IdentityHypothesis( label, p, d, blocksize_s )
             obj = obj@Hypothesis();
             obj.label = label;
             obj.p = p;
+            obj.d = d;
             obj.concernsBlocksize_s = blocksize_s;
         end
         
