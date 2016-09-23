4795aeaf0e1fe27035a0cb9c1e25e9a5611d5538
Christopher Schymura <christopher.schymura@rub.de>
2016-09-23 12:54:04 +0200

    Added functions for training data generation in GenderRecognitionKS.

--- src/knowledge_sources/GenderRecognitionKS.m
+++ src/knowledge_sources/GenderRecognitionKS.m
@@ -26,12 +26,96 @@ classdef GenderRecognitionKS < AuditoryFrontEndDepKS
     properties ( Access = private )
         basePath                % Path where extracted features and trained
                                 % classifiers should be stored.
+        pathToDataset           % Path to audio data for training and test.
     end
     
     properties ( Constant, Hidden )
         BLOCK_SIZE_SEC = 0.5;   % This KS works with a constant block size
                                 % of 500ms.
     end
+    
+    methods ( Static, Hidden )
+        function downloadGridCorpus()
+            % DOWNLOADGRIDCORPUS Automatically downloads the endpointed 
+            %   audio files of the GRID audiovisual sentence corpus from
+            %   http://spandh.dcs.shef.ac.uk/gridcorpus/.
+            
+            % Check if folder for storing the GRID corpus exists and create
+            % one if not.
+            pathToGridCorpus = fullfile( db.path(), 'sound_databases', ...
+                'grid_corpus' );
+            
+            if ~exist( pathToGridCorpus, 'dir' )
+                mkdir( pathToGridCorpus );
+            end
+            
+            % Number of speakers in GRID corpus is fixed to 34.
+            NUM_SPEAKERS = 34;
+            
+            for speakerIdx = 1 : NUM_SPEAKERS
+                % Check if files for current speaker have been downloaded.
+                if ~exist( fullfile(pathToGridCorpus, ...
+                        ['s', num2str(speakerIdx)]), 'dir' )
+                    
+                    disp(['GridCorpus::Downloading files for speaker ', ...
+                        num2str(speakerIdx), ' ...']);
+                    
+                    % Assemble download URL for current speaker.
+                    url = ['http://spandh.dcs.shef.ac.uk/gridcorpus/s', ...
+                        num2str(speakerIdx), '/audio/s', num2str(speakerIdx), '.tar'];
+                    
+                    % Download and unpack audio files for current speaker.
+                    untar( url, pathToGridCorpus );
+                    
+                    % Get path to audio files and perform sampling rate
+                    % conversion.
+                    pathToSpeakerFiles = fullfile(pathToGridCorpus, ...
+                        ['s', num2str(speakerIdx)]);                    
+                    fileList = getFiles( pathToSpeakerFiles, 'wav' );
+                    
+                    for file = fileList
+                        info = audioinfo( fullfile(pathToSpeakerFiles, file{:}) );
+                        
+                        % Resample if necessary
+                        if info.SampleRate ~= 44100
+                            disp(['GridCorpus::Processing file ', file{:}, ...
+                                ' for speaker ', num2str(speakerIdx), ' ...']);
+                            
+                            [signal, fs] = audioread( fullfile(pathToSpeakerFiles, file{:}) );
+                            signal = resample( signal, 44100, fs );
+                            
+                            % Get number of replicates.
+                            numReplicates = ceil( 44100 / length(signal) );
+                            
+                            % Replicate signal and truncate to desired
+                            % length of 1s.
+                            signal = repmat( signal(:), numReplicates, 1 );
+                            signal = signal( 1 : 44100 );
+                            
+                            audiowrite( fullfile(pathToSpeakerFiles, file{:}), ...
+                                signal, 44100 );
+                        end
+                    end
+                end
+            end
+        end
+        
+        function replicatedSignal = replicateSignal( signal, ...
+                signalLength, samplingRate )
+            %REPLICATESIGNAL Summary of this function goes here
+            %   Detailed explanation goes here
+            
+            % Get desired number of samples
+            numSamples = ceil( signalLength * samplingRate );
+            
+            % Get number of replicates
+            numReplicates = ceil( numSamples / length(signal) );
+            
+            % Replicate signal and truncate to desired length
+            replicatedSignal = repmat( signal(:), numReplicates, 1 );
+            replicatedSignal = replicatedSignal( 1 : numSamples );            
+        end
+    end    
 
     methods ( Access = public )
         function obj = GenderRecognitionKS()
@@ -66,11 +150,21 @@ classdef GenderRecognitionKS < AuditoryFrontEndDepKS
             
             % Get path to stored features and models. If such a directory
             % does not exist, it will be created.
-            obj.basePath = fullfile( xml.dbTmp, 'GenderRecognitionKS' );
+            obj.basePath = fullfile( db.tmp(), 'GenderRecognitionKS' );
             
             if ~exist( obj.basePath, 'dir' )
                 mkdir( obj.basePath );
+            end
+            
+            % Get path where audio files rendered are stored.
+            obj.pathToDataset = fullfile( obj.basePath, 'data' );
+            
+            if ~exist( obj.pathToDataset, 'dir' )
+                mkdir( obj.pathToDataset );
             end            
+            
+            obj.downloadGridCorpus();
+            obj.generateDataset();
         end
 
         function [bExecute, bWait] = canExecute( obj )
@@ -86,6 +180,92 @@ classdef GenderRecognitionKS < AuditoryFrontEndDepKS
                 obj.BLOCK_SIZE_SEC, obj.BLOCK_SIZE_SEC, false );
             spectralFeatures = obj.getNextSignalBlock( 3, ...
                 obj.BLOCK_SIZE_SEC, obj.BLOCK_SIZE_SEC, false );            
+        end
+    end
+    
+    methods ( Access = private )
+        function generateDataset( obj )
+            % GENERATEDATASET
+            
+            pathToGridCorpus = fullfile( db.path(), 'sound_databases', ...
+                'grid_corpus' );
+            
+            % Specify speakers for training and test sets.
+            femaleSpeakers = {'s4', 's7', 's11', 's15', 's16', 's18', 's20', ...
+                's21', 's22', 's23', 's24', 's25', 's29', 's31', 's33', 's34'};
+            maleSpeakers = {'s1', 's2', 's3', 's5', 's6', 's8', 's9', 's10', ...
+                's12', 's13', 's14', 's17', 's19', 's26', 's27', 's28', 's30', 's32'};
+            trainSet = {femaleSpeakers{1 : end - 2}, maleSpeakers{1 : end - 4}};
+            testSet = {femaleSpeakers{end - 2 : end}, maleSpeakers{end - 2 : end}};
+            
+            % Initialize the binaural simulator and fix all simulation parameters that
+            % will not change during data generation.
+            sim = simulator.SimulatorConvexRoom();
+            
+            sim.set( ...
+                'Renderer', @ssr_binaural, ...
+                'SampleRate', 44100, ...
+                'MaximumDelay', 0.05, ...
+                'PreDelay', 0.0, ...
+                'LengthOfSimulation', 1, ...    % Fixed to one second here.
+                'Sources', {simulator.source.Point()}, ...
+                'Sinks', simulator.AudioSink(2), ...
+                'HRIRDataset', simulator.DirectionalIR('impulse_responses/qu_kemar_anechoic/QU_KEMAR_anechoic_3m.sofa'), ...
+                'Verbose', false );
+            
+            sim.Sources{1}.set( ...
+                'Name', 'Speech', ...
+                'AudioBuffer', simulator.buffer.Ring, ...
+                'Volume', 1, ...
+                'Position', [cosd(0); sind(0); 1.75] );
+            
+            sim.Sinks.set( ...
+                'Position', [0; 0; 1.75], ...   % Cartesian position of the dummy head
+                'Name', 'DummyHead' );          % Identifier of the audio sink.
+            
+            for currentSet = {'train', 'test'}
+                if strcmp( currentSet{:}, 'train' )
+                    speakerIds = trainSet;
+                else
+                    speakerIds = testSet;
+                end
+                numSpeakers = length(speakerIds);
+                
+                for speaker = speakerIds
+                    % Get gender of current speaker.
+                    if any( strcmp(speaker{:}, femaleSpeakers) )
+                        gender = 'female';
+                    else
+                        gender = 'male';
+                    end
+                    
+                    % Check if folder for storing audio data exists.
+                    pathToAudioFiles = fullfile( obj.pathToDataset, ...
+                        currentSet{:}, gender );
+                    
+                    if ~exist( pathToAudioFiles, 'dir' )
+                        mkdir( pathToAudioFiles );
+                    end
+                    
+                    % Get all audio files for current speaker.
+                    listOfAudioFiles = getFiles( fullfile( ...
+                        pathToGridCorpus, speaker{:}), 'wav' );
+                    
+                    for file = listOfAudioFiles
+                        [~, filename] = fileparts( file{:} );
+                        processedFileName = ...
+                            [speaker{:}, '_', filename, '_', gender, '.wav'];
+                        pathToFile = fullfile(pathToAudioFiles, processedFileName);
+                        
+                        if ~exist( pathToFile, 'file' )
+                            % Add audio file to simulator.
+                            set( sim.Sources{1}.AudioBuffer, ...
+                                'File', fullfile(pathToGridCorpus, speaker{:}, file{:}) );    
+                            sim.init();
+                        end
+                    end
+                end
+            end
         end
     end
 end
