524f0369376c99791cb6d3c617363103b9cbd39e
Christopher <christopher.schymura@rub.de>
2017-01-18 20:31:28 +0100

    Final fixes in nav ks.

--- src/knowledge_sources/RobotNavigationKS.m
+++ src/knowledge_sources/RobotNavigationKS.m
@@ -9,8 +9,9 @@ classdef RobotNavigationKS < AbstractKS
         robotPositions = [
             %95, 97.5; % Outside kitchen
             91, 98; % Kitchen
-            91, 102;  % Bed room
-            86, 102]; % Living room
+            %91, 102;  % Bed room
+            87, 102]; % Living room
+        idlocIdx = 0;
     end
 
     methods
@@ -25,6 +26,8 @@ classdef RobotNavigationKS < AbstractKS
             else
                 obj.targetSource = [];
             end
+            
+            obj.invocationMaxFrequency_Hz = 1;
         end
 
         function setTargetSource(obj, targetSource)
@@ -33,70 +36,56 @@ classdef RobotNavigationKS < AbstractKS
 
         function [bExecute, bWait] = canExecute(obj)
             bWait = false;
+            bExecute = false;
             hyp = obj.blackboard.getLastData('singleBlockObjectHypotheses');
-            bExecute = ~isempty(hyp);
+            if ~isempty(hyp)
+                if ~isempty(obj.targetSource)
+                    idloc = hyp.data;
+                    idx = strcmp({idloc(:).label}, obj.targetSource);
+                    if max(idx) > 0 && any(cell2mat({idloc(idx).d}) == 1) 
+                        bExecute = true;
+                        obj.idlocIdx =  argmax(cell2mat({idloc.p}));
+                    end
+                end
+            end
         end
 
         function execute(obj)
             
-            if obj.movingScheduled 
-                % robot is moving
-                [~, statusId] = obj.robot.getNavigationState;
-                if statusId == 3
-                    % Reached target pos
-                    obj.movingScheduled = false;
-                else
-                    % Still moving
-                end
-            else
-                % Robot is not moving
-                % Let us get it to move
-                hyp = obj.blackboard.getLastData('singleBlockObjectHypotheses');
- 
-                idloc = hyp.data;
-                bMove = false;
-                if ~isempty(obj.targetSource)
-                    idx = strcmp({idloc(:).label}, obj.targetSource);
-                    if max(idx) > 0 && any(cell2mat({idloc(idx).d}) == 1)
-                        bMove = true;
-                        idx =  argmax(cell2mat({idloc.p}));
-                    end
-                end
-                if bMove
-                    % Now we have identified the target source. We want to
-                    % move the robot towards the source
 
-                    % idloc(idx).loc is source location relative to head
-                    targetLocBase = idloc(idx).loc + obj.robot.getCurrentHeadOrientation;
-                    [posX, posY, theta] = obj.robot.getCurrentRobotPosition;
-                    nRobotPositions = size(obj.robotPositions,1);
-                    
-                    relativeAngles = zeros(nRobotPositions, 1);
-                    for idx = 1 : nRobotPositions
-                        currentTargetPos = obj.robotPositions(idx, :);
-                        relativeAngles(idx) = atan2(posY - currentTargetPos(2), ...
-                            posX - currentTargetPos(1)); 
-                    end
-                    
-                    distances = 1 - cos(relativeAngles - targetLocBase);
-                    [~, bestPosIdx] = min(distances);
-                    
-%                     while true
-%                         idxLoc = randperm(nRobotPositions,1);
-%                         dist = sqrt((posX-obj.robotPositions(idxLoc,1))^2 + (posY-obj.robotPositions(idxLoc,2))^2);
-%                         if dist > 1
-%                             break;
-%                         end
-%                     end
-                    
-                    % Need to work out which angle to move to
-                    obj.robot.moveRobot(obj.robotPositions(bestPosIdx,1), obj.robotPositions(bestPosIdx,2), theta, 'absolute');
-                     obj.movingScheduled = true;
-                else
-                    % Not identified the target
-                    % Stay put
-                end
+            % Robot is not moving
+            % Let us get it to move
+            hyp = obj.blackboard.getLastData('singleBlockObjectHypotheses');
+ 
+            idloc = hyp.data;
+               
+            % Now we have identified the target source. We want to
+            % move the robot towards the source
+
+            % idloc(idx).loc is source location relative to head
+            targetLocBase = idloc(obj.idlocIdx).loc + obj.robot.getCurrentHeadOrientation;
+            [posX, posY, theta] = obj.robot.getCurrentRobotPosition;
+            nRobotPositions = size(obj.robotPositions,1);
+
+            relativeAngles = zeros(nRobotPositions, 1);
+            for idx = 1 : nRobotPositions
+                currentTargetPos = obj.robotPositions(idx, :);
+                relativeAngles(idx) = atan2d(currentTargetPos(2) - posY, ...
+                    currentTargetPos(1) - posX); 
             end
+
+            distances = 1 - cosd(relativeAngles - targetLocBase);
+            [~, bestPosIdx] = min(distances);
+
+            % Check if the target position is less than 1 metre away from
+            % the current position and stay put if yes
+            distMetres = sqrt((posX-obj.robotPositions(bestPosIdx,1))^2 + (posY-obj.robotPositions(bestPosIdx,2))^2);
+            if distMetres > 1
+                % Need to work out which angle to move to
+                obj.robot.moveRobot(obj.robotPositions(bestPosIdx,1), obj.robotPositions(bestPosIdx,2), theta, 'absolute');
+            end
+            %obj.movingScheduled = true;
+               
             notify(obj, 'KsFiredEvent', BlackboardEventData( obj.trigger.tmIdx ));
         end
         
