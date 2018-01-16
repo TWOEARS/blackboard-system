4e425b7e1590c2ff4b3aa748f73cb8a64a39f37c
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2018-01-16 16:24:26 +0100

    StreamSegregationKS.m: only calculate likelihoods when segregating into more than one stream... (i.e. when actually segregating.)

--- src/knowledge_sources/StreamSegregationKS.m
+++ src/knowledge_sources/StreamSegregationKS.m
@@ -141,17 +141,21 @@ classdef StreamSegregationKS < AuditoryFrontEndDepKS
                         locData.azimuths(locSortedAzmIdxs(azimuthIdx)) );
                 end
             end
-            likelihoods = zeros( size(itds, 1), size(itds, 2), numAzimuths );
-            for azimuthIdx = 1 : numAzimuths
-                likelihoods(:, :, azimuthIdx) = ...
-                    obj.observationModel.computeLikelihood( ...
-                    itds, ilds, refAzm(azimuthIdx) / 180 * pi );
+            if numAzimuths == 1
+                softMasks = ones( size( itds ) );
+            else
+                likelihoods = zeros( size(itds, 1), size(itds, 2), numAzimuths );
+                for azimuthIdx = 1 : numAzimuths
+                    likelihoods(:, :, azimuthIdx) = ...
+                        obj.observationModel.computeLikelihood( ...
+                        itds, ilds, refAzm(azimuthIdx) / 180 * pi );
+                end
+                
+                % Normalize likelihoods and put hypotheses on the blackboard.
+                likelihoodSum = squeeze( sum(permute(likelihoods, [3 2 1]),1) )';
+                likelihoodSum(likelihoodSum == 0) = 1; % do not divide by 0
+                softMasks = bsxfun( @rdivide, likelihoods, likelihoodSum );
             end
-            
-            % Normalize likelihoods and put hypotheses on the blackboard.
-            likelihoodSum = squeeze( sum(permute(likelihoods, [3 2 1]),1) )';
-            likelihoodSum(likelihoodSum == 0) = 1; % do not divide by 0
-            softMasks = bsxfun( @rdivide, likelihoods, likelihoodSum );
             numSoftMasks = size( softMasks, 3 );
             
             afeData = obj.getAFEdata();
